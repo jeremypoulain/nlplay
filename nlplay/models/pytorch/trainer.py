Fixed / Added Softmax linear classifier.Added option to store LRFinder output.

nlplay/models/pytorch/trainer.py
@@ -1,8 +1,9 @@
 import logging
 import os
 import time
-from pathlib import Path
 import torch
+from datetime import datetime
+from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
 from nlplay.models.pytorch.lr_finder import LRFinder
 from nlplay.models.pytorch.utils import set_seed, get_gpu_info
@@ -26,7 +27,7 @@ class PytorchModelTrainer(object):
         model_output_folder: Path = "",
         checkpoint_file_suffix: str = "",
         early_stopping=True,
-        early_stopping_patience: int = 3
+        early_stopping_patience: int = 3,
     ):
 
         self.model = model
@@ -44,6 +45,7 @@ class PytorchModelTrainer(object):
         self.test_dl = None
         self.val_dl = None
 
+        self.train_loss_hist = []
         self.apply_early_stopping = early_stopping
         self.early_stop = False
         self.best_score = None
@@ -80,8 +82,14 @@ class PytorchModelTrainer(object):
             lr_finder = LRFinder(
                 self.model, self.optimizer, criterion=self.criterion, device=device
             )
-            lr_finder.range_test(self.train_dl, end_lr=100, num_iter=100)
-            lr_finder.plot(show=False, folder_path="../../")
+            lr_finder.range_test(self.train_dl, start_lr=10e-6, end_lr=1, num_iter=100)
+            lr_finder.plot(
+                show=False,
+                output_path="LR_finder_{}_{}.png".format(
+                    self.model.__class__.__name__,
+                    datetime.now().strftime("%Y%m%d_%H%M%S"),
+                ),
+            )
             logging.info("LR Finder Run Completed....")
 
         # Checking the dataloaders
@@ -128,8 +136,9 @@ class PytorchModelTrainer(object):
                 self.optimizer.zero_grad()
 
                 # forward + backward + optimize
-                outputs = self.model(batch_train_data,)
+                outputs = self.model(batch_train_data)
                 loss = self.criterion(outputs, batch_train_labels)
+                self.train_loss_hist.append(loss.item())
                 loss.backward()
                 self.optimizer.step()
                 if self.lr_scheduler is not None:
@@ -206,6 +215,8 @@ class PytorchModelTrainer(object):
             self.model.state_dict(),
             os.path.join(
                 self.model_output_folder,
-                "{}_checkpoint_{}.pt".format(self.model.__class__.__name__, self.checkpoint_file_suffix),
+                "{}_checkpoint_{}.pt".format(
+                    self.model.__class__.__name__, self.checkpoint_file_suffix
+                ),
             ),
         )

nlplay/models/pytorch/utils.py
@@ -60,7 +60,7 @@ def embeddings_to_cosine_similarity_matrix(embedding: torch.Tensor):
     Authors  : Dillon Erb - https://github.com/dte
     Papers   : ---
     Source   : https://gist.github.com/dte/e600bb76e72854379f4a306c1873f2c2#file-vectorized_cosine_similarities-py
-  """
+    """
     dot = embedding @ embedding.t()
     norm = torch.norm(embedding, 2, 1)
     x = torch.div(dot, norm)
@@ -70,7 +70,12 @@ def embeddings_to_cosine_similarity_matrix(embedding: torch.Tensor):
 
 class LockedDropout(torch.nn.Module):
     """
-    Implementation of locked (or variational) dropout. Randomly drops out entire parameters in embedding space.
+    Title    : A Theoretically Grounded Application of Dropout in Recurrent Neural Networks
+    Authors  : Yarin Gal, Zoubin Ghahramani
+    Papers   : https://arxiv.org/pdf/1512.05287.pdf
+    Source   : https://github.com/flairNLP/flair/blob/master/flair/nn.py
+    Note     : Implementation of locked (or variational) dropout.
+               Randomly drops out entire parameters in embedding space.
     """
 
     def __init__(self, dropout_rate=0.5, batch_first=True, inplace=False):
@@ -99,7 +104,12 @@ class LockedDropout(torch.nn.Module):
 
 class WordDropout(torch.nn.Module):
     """
-    Implementation of word dropout. Randomly drops out entire words (or characters) in embedding space.
+    Title    : A Theoretically Grounded Application of Dropout in Recurrent Neural Networks
+    Authors  : Yarin Gal, Zoubin Ghahramani
+    Papers   : https://arxiv.org/pdf/1512.05287.pdf
+    Source   : https://github.com/flairNLP/flair/blob/master/flair/nn.py
+    Note     : Implementation of word dropout.
+               Randomly drops out entire words (or characters) in embedding space.
     """
 
     def __init__(self, dropout_rate=0.05, inplace=False):
@@ -110,9 +120,7 @@ class WordDropout(torch.nn.Module):
     def forward(self, x):
         if not self.training or not self.dropout_rate:
             return x
-
         m = x.data.new(x.size(0), x.size(1), 1).bernoulli_(1 - self.dropout_rate)
-
         mask = torch.autograd.Variable(m, requires_grad=False)
         return mask * x
 

scripts/tfidf_linear_train_script.py
@@ -5,7 +5,7 @@ from torch import nn
 
 from nlplay.data.cache import DSManager, DS
 from nlplay.features.text_cleaner import base_cleaner
-from nlplay.models.pytorch.classifiers.linear import LinearModel
+from nlplay.models.pytorch.classifiers.linear import SMLinearModel
 from nlplay.models.pytorch.dataset import CSRDatasetGenerator
 from nlplay.models.pytorch.trainer import PytorchModelTrainer
 
@@ -35,7 +35,7 @@ train_ds, val_ds = ds.from_csv(train_file=train_csv, val_file=test_csv, ngram_ra
                                sublinear_tf=sublinear_tf, stop_words=stop_words,
                                preprocess_func=base_cleaner, preprocess_ncore=3)
 
-model = LinearModel(input_size=ds.vocab_size, num_classes=ds.num_classes)
+model = SMLinearModel(input_size=ds.vocab_size, num_classes=ds.num_classes)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 