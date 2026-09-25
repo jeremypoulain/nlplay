c1acbd77026c14a241690b63908c83db91e7f5a4
jeremypoulain <13520558+jeremypoulain@users.noreply.github.com>
2026-09-25 08:34:10 +0200

    Tune the HashedFastText script and use FastTextDatasetGenerator
    - Adam (SparseAdam for the embeddings) lr 0.01, 5 epochs: 0.911 on a
      20% holdout of the train set, 0.905 on the IMDB test set instead of
      0.612, SGD lr 0.5 did not learn with the minibatch mean loss
    - Word bigrams, dim 50: trigrams, dim 100 and character n-grams no better
    - ModifiedHuberLoss option, same accuracy with lr 0.003
    - Data preparation with FastTextDatasetGenerator, black formatting and
      run summary as in the other scripts

--- scripts/fasttext_hashed_train_script.py
+++ scripts/fasttext_hashed_train_script.py
@@ -1,55 +1,109 @@
 import logging
-import pandas as pd
 from torch import nn
 from nlplay.data.cache import DSManager, DS
-from nlplay.features.fasttext_features import FastTextFeaturizer, read_word_vectors
 from nlplay.features.text_cleaner import *
-from nlplay.models.pytorch.classifiers.fasttext_hashed import HashedFastText, FastTextDataset, fasttext_optimizer
+from nlplay.models.pytorch.classifiers.fasttext_hashed import (
+    HashedFastText,
+    fasttext_optimizer,
+)
+from nlplay.models.pytorch.dataset import FastTextDatasetGenerator
+from nlplay.models.pytorch.losses import ModifiedHuberLoss
 from nlplay.models.pytorch.trainer import PytorchModelTrainer
-from nlplay.utils.parlib import parallelApply
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    format="%(asctime)s %(message)s", level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 # Input data files
 ds = DSManager(DS.IMDB.value)
 train_csv, test_csv, val_csv = ds.get_partition_paths()
 
-# Inputs & Model Parameters, fastText supervised style: word bigrams, no dropout, SGD with linear decay
+# Inputs & Model Parameters
+# Tuned on a 20% holdout of the train set. Sparse embeddings, so "adam" trains them with
+# SparseAdam. With SGD (fastText style), the minibatch mean loss needs lr = 20, 20 epochs
+# for 0.900 on the holdout vs 0.911 with Adam.
 num_epochs = 5
 batch_size = 32
 word_ngrams = 2
-minn, maxn = 0, 0   # e.g. 3, 6 to add character n-grams, useful for noisy text / rare words
+minn, maxn = 0, 0  # e.g. 3, 6 for character n-grams, lower and 4x slower on IMDB
 bucket = 2000000
 embedding_size = 50
-loss = "softmax"         # "hs" for the hierarchical softmax, worth it with many classes
-optimizer_name = "sgd"   # fastText style, or e.g. "adamw" with lr = 1e-2 (SparseAdam for the sparse embeddings)
-lr = 0.5
-pretrained_vec_file = None   # e.g. a fastText .vec file of dimension embedding_size
+loss = "softmax"  # "hs" for the hierarchical softmax, worth it with many classes
+criterion_name = "cross_entropy"  # or "modified_huber", same accuracy with lr = 0.003
+optimizer_name = "adam"
+lr = 0.01
+pretrained_vec_file = None  # e.g. a fastText .vec file of dimension embedding_size
 num_workers = 1
 
 # Data preparation
-df_train, df_val = pd.read_csv(train_csv), pd.read_csv(test_csv)
-train_texts = parallelApply(df_train[df_train.columns[0]], base_cleaner, 3).tolist()
-val_texts = parallelApply(df_val[df_val.columns[0]], base_cleaner, 3).tolist()
-pretrained_words, pretrained_vectors = read_word_vectors(pretrained_vec_file) if pretrained_vec_file else (None, None)
-featurizer = FastTextFeaturizer(word_ngrams=word_ngrams, minn=minn, maxn=maxn, bucket=bucket)
-featurizer.fit(train_texts, pretrained_words=pretrained_words)
-train_ds = FastTextDataset(featurizer.transform(train_texts), df_train[df_train.columns[1]].to_numpy(),
-                           featurizer.num_features)
-val_ds = FastTextDataset(featurizer.transform(val_texts), df_val[df_val.columns[1]].to_numpy(),
-                         featurizer.num_features)
-num_classes = int(max(train_ds.labels.max(), val_ds.labels.max())) + 1
+ds = FastTextDatasetGenerator()
+train_ds, val_ds = ds.from_csv(
+    train_file=train_csv,
+    val_file=test_csv,
+    preprocess_func=base_cleaner,
+    preprocess_ncore=3,
+    word_ngrams=word_ngrams,
+    minn=minn,
+    maxn=maxn,
+    bucket=bucket,
+    pretrained_vec_file=pretrained_vec_file,
+)
 
-model = HashedFastText(featurizer.num_features, num_classes, embedding_size=embedding_size, sparse=True,
-                       loss=loss, class_counts=train_ds.labels.bincount(minlength=num_classes).tolist())
+model = HashedFastText(
+    ds.num_features,
+    ds.num_classes,
+    embedding_size=embedding_size,
+    sparse=True,
+    loss=loss,
+    class_counts=ds.class_counts,
+)
 if pretrained_vec_file:
-    model.load_word_vectors(featurizer, pretrained_words, pretrained_vectors)
-# with loss="hs" the trainer uses model.training_loss, the criterion is not used for training
-criterion = nn.CrossEntropyLoss() if loss == "softmax" else nn.NLLLoss()
-optimizer, scheduler = fasttext_optimizer(model, lr=lr, total_steps=num_epochs * -(-len(train_ds) // batch_size),
-                                          optimizer=optimizer_name)
+    model.load_word_vectors(ds.featurizer, ds.pretrained_words, ds.pretrained_vectors)
 
-trainer = PytorchModelTrainer(model, criterion, optimizer, lr_scheduler=scheduler,
-                              train_ds=train_ds, val_ds=val_ds,
-                              batch_size=batch_size, n_workers=num_workers, epochs=num_epochs)
+# with loss="hs" the trainer uses model.training_loss, the criterion is not used for training
+if loss == "hs":
+    criterion = nn.NLLLoss()
+elif criterion_name == "modified_huber":
+    criterion = ModifiedHuberLoss()
+else:
+    criterion = nn.CrossEntropyLoss()
+optimizer, scheduler = fasttext_optimizer(
+    model,
+    lr=lr,
+    total_steps=num_epochs * -(-len(train_ds) // batch_size),
+    optimizer=optimizer_name,
+)
+
+trainer = PytorchModelTrainer(
+    model,
+    criterion,
+    optimizer,
+    lr_scheduler=scheduler,
+    train_ds=train_ds,
+    val_ds=val_ds,
+    batch_size=batch_size,
+    n_workers=num_workers,
+    epochs=num_epochs,
+)
 trainer.train_evaluate()
+
+# num_epochs = 5
+# batch_size = 32
+# word_ngrams = 2
+# minn, maxn = 0, 0
+# bucket = 2000000
+# embedding_size = 50
+# loss = "softmax"
+# criterion_name = "cross_entropy"
+# optimizer_name = "adam"
+# lr = 0.01
+# num_workers = 1
+# 2026-09-25 08:19:51 ------------------------------------------
+# 2026-09-25 08:19:51 ---              SUMMARY               ---
+# 2026-09-25 08:19:51 ------------------------------------------
+# 2026-09-25 08:19:51 Number of model parameters : 103907100
+# 2026-09-25 08:19:51 Total Training Time: 0m 13s
+# 2026-09-25 08:19:51 Total Time: 0m 13s
+# 2026-09-25 08:19:51 Best Epoch: 1 - Accuracy Score: 0.905480
+# 2026-09-25 08:19:51 ------------------------------------------
+# Early stop after epoch 4, test accuracy 0.899 to 0.901 after epoch 1, train accuracy 1.0
